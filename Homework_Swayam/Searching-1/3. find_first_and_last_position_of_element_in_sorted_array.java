2188df186ee29d0d819ba2702459bcebd53766ab
Sarthakk2503 <114071830+Sarthakk2503@users.noreply.github.com>
2024-03-08 22:44:52 +0530

    Update 3. find_first_and_last_position_of_element_in_sorted_array.java

--- Homework_Swayam/Searching-1/3. find_first_and_last_position_of_element_in_sorted_array.java
+++ Homework_Swayam/Searching-1/3. find_first_and_last_position_of_element_in_sorted_array.java
@@ -7,8 +7,9 @@ class Solution {
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(nums[mid]>target)
+            if(nums[mid]==target)
             {
+                i=mid;
                 end=mid-1;
             }
             else if(nums[mid]<target)
@@ -16,19 +17,26 @@ class Solution {
                 start=mid+1;
             }
             else
-            {
-                if(nums[end]>target)
-                end--;
-                else if(nums[start]<target)
-                start++;
-                else
-                {
-                    i=start;
-                    j=end;
-                    break;
-                }
-            }
+            end=mid-1;
         }
+        start=0;
+        end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                j=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else
+            end=mid-1;
+        }
+
         return new int[]{i,j};
     }
 }
